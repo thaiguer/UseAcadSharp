66cd89fb2e0ebe27d68f92a19c11d63a6e2dbe92
tgr <thaiguer.k@gmail.com>
2024-09-23 22:49:41 -0300

    initial_commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using ACadSharp;
+using ACadSharp.IO;
+
+Console.WriteLine("Hello, World!");
+
+string sourceFile = @"C:\dev\tablet.dwg";
+string targetFile = @"C:\dev\just a drawing222.dxf";
+
+using (DwgReader reader = new DwgReader(sourceFile))
+{
+    CadDocument doc = reader.Read();
+    CadDocument newDoc = new CadDocument();
+
+    foreach (var entity in doc.Entities)
+    {
+        doc.Entities.Remove(entity);
+        newDoc.Entities.Add(entity);
+    }
+
+    using (DwgWriter writer = new DwgWriter(targetFile, newDoc))
+    {
+        writer.Write();
+    }
+}
